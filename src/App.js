c1e3c5b865d8481b801b4f83d3182cc1170c3bc0
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-02-09 10:46:09 +0400

    react routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Cart from './pages/Cart';
 function App() {
-  return <Login />;
+  //TODO: Delete this Later
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path='/products/:category' element={<ProductList />} />
+      </Routes>
+      <Routes>
+        <Route path='/product/:id' element={<Product />} />
+      </Routes>
+      <Routes>
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+      <Routes>
+        <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route
+          path='/register'
+          element={user ? <Navigate to='/' /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   margin: 3px;
@@ -39,13 +40,16 @@ const Button = styled.button`
 `;
 
 const CategoryItem = ({ item }) => {
+  console.log('Item : ', item.cat);
   return (
     <Container>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.cat}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
     </Container>
   );
 };
